71b7aeb1570c65f7f4c15fbc521f70c47199151c
FlightLoong <1577886278@qq.com>
2022-01-10 18:42:32 +0800

    准备素材+安装 element

--- main/src/main.js
+++ main/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入 Element
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 将 Element 注册为插件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
